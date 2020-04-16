cd9d31bacfce6821d1eb97dcca5c9b293ee92a9e
Federico Ginosa <federico@menxit.com>
2020-04-16 18:28:25 +0200

    full address

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,18 +15,27 @@ const Header = styled.div`
 	align-items: center;
 	border-bottom: 1px dotted ${colors.darkGrey};
 	.address {
-		text-align: center;
+		align-items: center;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+	.address__copy {
 		background: ${colors.black};
 		color: ${colors.white};
 		padding: .5em;
 		border-radius: 3px;
-		display: block;
+		display: inline-flex;
 		text-decoration: none;
 		cursor: pointer;
 	}
-	.address:hover {
+	.address__copy:hover {
 		color: ${colors.white};
 		background: ${colors.darkBlack};
+	}
+	.address__full {
+		padding-top: .5em;
+		font-size: .8em;
 	}
 	.logout {
 		cursor: pointer;
@@ -79,9 +88,12 @@ export default () => {
 	return (
 		<Header>
 			<Logo width={50} />
-			<CopyToClipboard text={storage.address}>
-				<span className="address">{formatAddress(storage.address, 6)}</span>
-			</CopyToClipboard>
+			<div className="address">
+				<CopyToClipboard text={storage.address}>
+					<span className="address__copy">{formatAddress(storage.address, 6)}</span>
+				</CopyToClipboard>
+				<span className="address__full">{storage.address}</span>
+			</div>
 			<span className="logout" onClick={() => dispatch(lock())}>Logout</span>
 			{storage.message && <div className={`toast toast--${storage.type || "info"}`}>
 				{storage.message}
