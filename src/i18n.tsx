ff39dc52a728ba7efb7dfa69f0a21e1e8c7a0862
bingbinglee <62775041+bingbinglee@users.noreply.github.com>
2020-05-11 21:38:33 -0400

    Update i18n.tsx

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,10 +1,9 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-
-
 import translationEN from "../public/locales/en/translation.json";
 import translationHI from "../public/locales/hi/translation.json";
+import translationIT from "../public/locales/it/translation.json";
 
 // the translations
 const resources = {
@@ -13,7 +12,10 @@ const resources = {
   },
   hi: {
     translation: translationHI
-  }
+  },
+  it: {
+    translation: translationIT
+  },
 };
 
 
