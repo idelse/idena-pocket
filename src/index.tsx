b5e923f17d1febbf977aa7220f324fbe515b5236
Federico Ginosa <federico@menxit.com>
2020-08-23 10:35:30 +0200

    Add service worker #31

--- src/index.tsx
+++ src/index.tsx
@@ -71,7 +71,11 @@ if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		navigator.serviceWorker
 			.register('/service-worker.js')
-			.then(registration => console.log('SW registered: ', registration))
+			.then(registration =>
+				console.log(
+					"Spero che ritorni presto l'era del cinghiale bianco"
+				)
+			)
 			.catch(registrationError =>
 				console.log('SW registration failed: ', registrationError)
 			)
