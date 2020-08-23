406d27a82eae9e566b5b18035bca12fab5e6f0dd
Federico Ginosa <federico@menxit.com>
2020-08-23 10:20:04 +0200

    Add service worker #31

--- src/index.tsx
+++ src/index.tsx
@@ -69,8 +69,11 @@ ReactDOM.render(
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/service-worker.js')
+		navigator.serviceWorker
+			.register('/service-worker.js')
 			.then(registration => console.log('SW registered: ', registration))
-			.catch(registrationError => console.log('SW registration failed: ', registrationError))
+			.catch(registrationError =>
+				console.log('SW registration failed: ', registrationError)
+			)
 	})
 }
